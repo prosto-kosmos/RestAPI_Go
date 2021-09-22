d56bdc59242850591bdb2b404fb95ff690dc7916
prosto-kosmos <kr.mikki@yandex.ru>
2021-09-22 12:08:03 +0300

    #4

--- cmd/main.go
+++ cmd/main.go
@@ -5,17 +5,27 @@ import (
 	"github.com/prosto-kosmos/RestAPI_Go/pkg/handler"
 	"github.com/prosto-kosmos/RestAPI_Go/pkg/repository"
 	"github.com/prosto-kosmos/RestAPI_Go/pkg/service"
+	"github.com/spf13/viper"
 
 	"log"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing congigs: %s", err.Error())
+	}
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run("8000", handlers.InitRouters()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRouters()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
