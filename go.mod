422e7819fc989850e62e186ce995207ea3dfa6ee
prosto-kosmos <kr.mikki@yandex.ru>
2021-09-22 16:59:29 +0300

    #7

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/prosto-kosmos/RestAPI_Go
 go 1.13
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.7.4
 	github.com/go-playground/validator/v10 v10.9.0 // indirect
 	github.com/jmoiron/sqlx v1.3.4
--- pkg/service/auth.go
+++ pkg/service/auth.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/dgrijalva/jwt-go"
 	todo "github.com/prosto-kosmos/RestAPI_Go"
 	"github.com/prosto-kosmos/RestAPI_Go/pkg/repository"
 )
@@ -14,6 +15,11 @@ const (
 	signingKey = "qrkjk#4#%35FSFJlja#4353KSFjH"
 	tokenTTL   = 12 * time.Hour
 )
+
+type tokenClaims struct {
+	jwt.StandardClaims
+	UserId int `json:"user_id"`
+}
 
 type AuthService struct {
 	repo repository.Authorization
@@ -26,6 +32,23 @@ func NewAuthService(repo repository.Authorization) *AuthService {
 func (s *AuthService) CreateUser(user todo.User) (int, error) {
 	user.Password = generatePasswordHash(user.Password)
 	return s.repo.CreateUser(user)
+}
+
+func (s *AuthService) GenerateToken(username, password string) (string, error) {
+	user, err := s.repo.GetUser(username, generatePasswordHash(password))
+	if err != nil {
+		return "", err
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &tokenClaims{
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(tokenTTL).Unix(),
+			IssuedAt:  time.Now().Unix(),
+		},
+		user.Id,
+	})
+
+	return token.SignedString([]byte(signingKey))
 }
 
 func generatePasswordHash(password string) string {
