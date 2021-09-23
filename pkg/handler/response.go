a1ca2a6fe4dfc91edcbc987500ece477dadb4040
prosto-kosmos <kr.mikki@yandex.ru>
2021-09-23 15:26:23 +0300

    #10

--- pkg/handler/response.go
+++ pkg/handler/response.go
@@ -9,6 +9,10 @@ type ErrorResponse struct {
 	Message string `json:"message"`
 }
 
+type statusResponse struct {
+	Status string `json:"status"`
+}
+
 func newErrorResponse(c *gin.Context, statusCode int, message string) {
 	logrus.Errorf(message)
 	c.AbortWithStatusJSON(statusCode, ErrorResponse{message})
